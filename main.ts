2fe7160be5d7d4821513d5882b8295c52465096e
613S038\sse <1610576559@qq.com>
2016-09-21 13:44:12 +0800

    Finish Homework2

---
+++ main.ts
@@ -0,0 +1,17 @@
+function print(n:number):void{
+for (var i = 1; i <= num; i++) {
+    var str:string = "";
+    for (var j = 1; j <= num - i; j++) {
+         str = str +" ";
+         
+    }
+    for (var k = 1; k <= 2 * i - 1; k++) {
+         var star :string = "*";
+         str = str + star;
+    }
+    console.log(str);  
+}
+}
+
+ var num:number = process.argv[2]; 
+print(num);
